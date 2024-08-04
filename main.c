cc876aada4753a60d708cc84dc30b329b3f7e1a9
Odi Ogali <odi.ogali1380@gmail.com>
2024-08-03 20:58:36 -0600

    Bug where there, after returning from alternate screen, is an empty line before next prompt persists. Also cursor doesn't completely start in the very top left corner of the screen.

--- main.c
+++ main.c
@@ -1,31 +1,57 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <termios.h>
 #include "tc.h"
 
-struct termios orig_termios;
+struct termios orig_termios; // will hold original terminal settings to revert back to it
+
+void die(const char *s){
+  perror(s);
+  exit(1);
+}
 
 void disableRawMode(){
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) // set terminal setting to old settings
+    die("tcsettattr"); 
 }
 
 void enableRawMode(){
   struct termios raw; // struct is created that will hold terminal attributes
 
-  tcgetattr(STDIN_FILENO, &orig_termios); // get attributes and store in variable
-  raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON); // bitwise operation to turn off ECHO feature that prints what you're typing
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) // get attributes and store in variable
+    die("tcgetattr"); 
+  raw = orig_termios; // structs do NOT behave like objects in Java
+  raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP); // IXON: ctrl-s/q - used for pausing + resuming data transmission
+  // ICRNL refers to carriage return and newline - terminal makes them the same ASCII code (no good) -> ctrl-m != ctrl-j
+  raw.c_oflag &= ~(OPOST); // turn off all post processing of our input
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); // bitwise operation to turn off ECHO feature - prints text you type
+  // ISIG turns off ctrl-c and ctrl-z functionality in terminal
+  // IEXTEN fixes ctrl-v in some terminals where it waits for another character and then sends it, also fixes ctrl-o in Macs
+  // Turn off remaining miscellanous flags (including BRKINT, INPCK, ISTRIP):
+  raw.c_cflag |= (CS8);
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // update terminal attributes
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) // update terminal attributes
+    die("tcsetattr");
 }
 
 int main(){
 
   tc_enter_alt_screen();
+  tc_clear_screen();
+  tc_move_cursor(0, 0);
   enableRawMode();
 
   char c;
-  while(read(STDIN_FILENO, &c, 1) == 1){
+  while(read(STDIN_FILENO, &c, 1) == 1){ // may need to do error handling for read??
+    if (iscntrl(c)){
+      printf("%d\r\n", c);
+    } else {
+      printf("%d (%c)\r\n", c, c);
+    }
+
     if (c == 'q'){
       disableRawMode();
       tc_exit_alt_screen();
--- tc.h
+++ tc.h
@@ -1,3 +1,9 @@
 #include <stdio.h>
-#define tc_enter_alt_screen() puts("\033[?1049h\033[H")
+#define tc_enter_alt_screen() puts("\033[?1049h")
 #define tc_exit_alt_screen() puts("\033[?1049l")
+#define tc_clear_screen() puts("\x1B[2J")
+#define tc_move_cursor(X, Y) printf("\033[%d;%dH", Y, X)
+#define tc_move_cursor_left()
+#define tc_move_cursor_right()
+#define tc_move_cursor_up()
+#define tc_move_cursor_down()

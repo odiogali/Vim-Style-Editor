4d277434542d0b1dd00ea0500529139d3b6cbef4
Odi Ogali <odi.ogali1380@gmail.com>
2024-08-05 00:07:50 -0600

    Any progress is good progress.

--- main.c
+++ main.c
@@ -6,6 +6,11 @@
 #include <termios.h>
 #include "tc.h"
 
+enum Mode {
+  Normal = 0,
+  Insert = 1, 
+};
+
 struct termios orig_termios; // will hold original terminal settings to revert back to it
 
 void die(const char *s){
@@ -23,6 +28,7 @@ void enableRawMode(){
 
   if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) // get attributes and store in variable
     die("tcgetattr"); 
+
   raw = orig_termios; // structs do NOT behave like objects in Java
   raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP); // IXON: ctrl-s/q - used for pausing + resuming data transmission
   // ICRNL refers to carriage return and newline - terminal makes them the same ASCII code (no good) -> ctrl-m != ctrl-j
@@ -37,12 +43,25 @@ void enableRawMode(){
     die("tcsetattr");
 }
 
+int handleEvent(char event, enum Mode mode){
+  if (mode == 0) {
+    switch (event){
+
+    }
+  } else {
+
+  }
+  return 0;
+}
+
 int main(){
 
   tc_enter_alt_screen();
   tc_clear_screen();
-  tc_move_cursor(0, 0);
+  int cx = 0, cy = 0; // cursor position
+  tc_move_cursor(cx, cy);
   enableRawMode();
+  enum Mode mode = Normal;
 
   char c;
   while(read(STDIN_FILENO, &c, 1) == 1){ // may need to do error handling for read??
